8de06e1d04813e57f417571f2fee16c1592923a0
jandomhof <j.h.domhof@outlook.com>
2022-08-09 12:52:24 +0200

    test app.js initialisation

--- src/App.js
+++ src/App.js
@@ -12,49 +12,50 @@ const address = "0xBAB9b565218d8B7517eFE2Cc35dB41EFfFbe0b9E";
 const html = document.getElementById("html");
 
 function App() {
-  // const [accounts, setAccounts] = useState([]);
-  // const [status, setStatus] = useState(0);
-  // const [tokenId, setTokenId] = useState(0);
-  // const { width, height } = useWindowDimensions();
+  const [accounts, setAccounts] = useState([]);
+  const [status, setStatus] = useState(0);
+  const [tokenId, setTokenId] = useState(0);
+  const { width, height } = useWindowDimensions();
 
-  // if (width > 640) {
-  //   if (!html.classList.contains("computer")) {
-  //     html.classList.add("computer");
-  //     html.classList.remove("mobile");
-  //   }
-  // } else {
-  //   if (!html.classList.contains("mobile")) {
-  //     html.classList.add("mobile");
-  //     html.classList.remove("computer");
-  //   }
-  // }
+  if (width > 640) {
+    if (!html.classList.contains("computer")) {
+      html.classList.add("computer");
+      html.classList.remove("mobile");
+    }
+  } else {
+    if (!html.classList.contains("mobile")) {
+      html.classList.add("mobile");
+      html.classList.remove("computer");
+    }
+  }
 
-  // useEffect(() => {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //   const contract = new ethers.Contract(address, notMafia.abi, provider);
-  //   contract.on("ChangedStatus", (newStatus) => {
-  //     setStatus(newStatus);
-  //   });
-  //   contract.on("Transfer", (_from, _to, amount) => {
-  //     setTokenId(tokenId + amount);
-  //   });
-  //   async function fetchFromContract() {
-  //     const currentStatus = await contract.status();
-  //     setStatus(currentStatus);
+  useEffect(() => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(address, notMafia.abi, provider);
+      contract.on("ChangedStatus", (newStatus) => {
+        setStatus(newStatus);
+      });
+      contract.on("Transfer", (_from, _to, amount) => {
+        setTokenId(tokenId + amount);
+      });
+      async function fetchFromContract() {
+        const currentStatus = await contract.status();
+        setStatus(currentStatus);
 
-  //     const currentTokenId = await contract.getCurrentTokenId();
-  //     setTokenId(currentTokenId);
-  //   }
-  //   fetchFromContract();
-  // }, []);
+        const currentTokenId = await contract.getCurrentTokenId();
+        setTokenId(currentTokenId);
+      }
+      fetchFromContract();
+    }
+  }, []);
 
   return (
-    <Text>THIS IS WITHIN APP.JS</Text>
-    // <Flex height={"100%"} direction="column" justify={"space-between"}>
-    //   <NavBar accounts={accounts} setAccounts={setAccounts} />
-    //   <Mint accounts={accounts} address={address} status={status} />
-    //   <FootBar address={address} tokenId={tokenId} />
-    // </Flex>
+    <Flex height={"100%"} direction="column" justify={"space-between"}>
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
+      <Mint accounts={accounts} address={address} status={status} />
+      <FootBar address={address} tokenId={tokenId} />
+    </Flex>
   );
 }
 
