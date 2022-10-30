416dfb00a543ff44cfca605dc19e0f8f92587f75
jandomhof <j.h.domhof@outlook.com>
2022-10-30 02:23:43 +0200

    updated whitelist

--- src/components/helpers/WhiteList.js
+++ src/components/helpers/WhiteList.js
@@ -5,9 +5,7 @@ window.Buffer = window.Buffer || require("buffer").Buffer;
 
 const addresses = [
   "0x0B76F2E7083c4FA0844d712A6018b42013Dc8Af7",
-  "0xad3EA2A02B83a492f9e512401851E158f614FAC9",
-  "0xDE8D5aE4c54e4De86AFeF3B9FE35B40D88194d61",
-  "0x44479d182144Aeb39Da931c9Ef69F478538972ea",
+  "0xe00b6A41E023f031C04B6DF13aDEe4dfbaeb2dDF",
 ];
 
 const leaves = addresses.map((x) => keccak256(x));
